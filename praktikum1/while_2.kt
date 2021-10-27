3a05a40995bbe3395ec1a656b3c4af58ec27919a
Rizki Rafiif <58683476+rizki-rafiif@users.noreply.github.com>
2021-10-27 19:02:57 +0700

    Add files via upload
    upload praktikum 1 matkul pemrograman perangkat bergerak

---
+++ praktikum1/while_2.kt
@@ -0,0 +1,10 @@
+package PraktikumAndroid.praktikum1
+
+fun main() {
+    // program do while counter juga
+    var count = 1
+    do {
+        println("Player number $count join the game")
+        count++
+    }while (count<6)
+}
