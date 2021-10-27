1c6114d2a06706a8f107fde382fd7d9e90c5a1f5
Rizki Rafiif <58683476+rizki-rafiif@users.noreply.github.com>
2021-10-27 19:04:20 +0700

    salah upload

--- praktikum1/Prodi.kt
+++
@@ -1,4 +0,0 @@
-package PraktikumAndroid.praktikum1
-
-open class Prodi {
-}
--- praktikum1/for_1.kt
+++
@@ -1,9 +0,0 @@
-package PraktikumAndroid.praktikum1
-
-fun main() {
-    // program for cara 1
-    var numb = 1.rangeTo(10) step 3     // 1 sampe 10 dengan skip tiap 3 angka
-    for (i in numb){
-        println("number of i now is $i")
-    }
-}
