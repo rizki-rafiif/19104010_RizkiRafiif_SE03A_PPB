98367dd19970da56e7a314cf18700814292f6147
Rizki Rafiif <58683476+rizki-rafiif@users.noreply.github.com>
2021-11-10 16:51:26 +0700

    upload pertemuan 3
    bisa di review mas :D

---
+++ pertemuan2/HelloITTelkomPurwokerto/app/build/generated/source/buildConfig/debug/com/rizkirafiif_19104010/helloittelkompurwokerto/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.rizkirafiif_19104010.helloittelkompurwokerto;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.rizkirafiif_19104010.helloittelkompurwokerto";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
